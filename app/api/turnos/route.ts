bc3ebeb02d6eb2680790dfd28e311c1cd4880423
Julian Cancelo <35230531@itbeltran.com.ar>
2025-10-17 12:56:39 -0300

    fix: Corregir campo tipo_transporte en API de turnos

--- app/api/turnos/route.ts
+++ app/api/turnos/route.ts
@@ -56,7 +56,7 @@ export async function GET(request: Request) {
           select: {
             id: true,
             nro_licencia: true,
-            tipo: true,
+            tipo_transporte: true,
           }
         })
 
